43c6dfdff81e33fa777856753fae6e347cb4ae35
Ishita Gambhir <ishita.gambhir28@gmail.com>
2019-07-13 02:53:52 +0530

    added 2b method 1

---
+++ 2B m1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	long long int num, i, factorial = 1;
+	printf("Enter number:\t");
+	fflush(stdin);
+	scanf("%lld", &num);
+	if(num >= 0){
+		for(i = 1; i <= num; i++){
+			factorial *= i;
+		}
+		printf("factorial of %d is %d\n", num, factorial);
+	}
+	else{
+		printf("Invalid number.\n");
+	}
+	return 0;
+}
